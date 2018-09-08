61b064ad42e14ab91efdd5e7bf19a3a931bd19a9
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-09-08 23:05:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,1 @@
- const Discord = require('discord.js');
-const epic = new Discord.Client();
 
-epic.on('ready',async () => {
-  epic.channels.find(ch => ch.id === "487045322929995777" && ch.type === 'voice').join();
-});
-
-// طبعا عشان الكود يشتغل بكفاءة لازم يكون البوت مفتوح 24 ساعة
-// البوت بيخرج برا الروم اذا الخادم قفل
-// جميع الحقوق محفوظة لسيرفر كودز
-
-epic.login("NDg3OTYxNzgyNzMyODQ5MTYz.DnVjOg.XN68jq1mW4padPXDxtybv-wyMvY")
