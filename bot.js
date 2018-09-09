95e42016c84055910a3d1e9d31dc7c5fb4314cd1
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-09-09 02:03:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -204,23 +204,6 @@ const Embed11 = new Discord.RichEmbed()
 eyad.channel.sendEmbed(Embed11).then(eyad => {eyad.delete(20000)})
     }
 })
-      client.on('message', ra3d = {
-var prefix = *;
-                        let args = ra3d.content.split( ).slice(1).join( )
-if(ra3d.content.startsWith(prefix + 'cc')) {
-    if(!args) return ra3d.channel.send('`يرجي اختيار كم لون `');
-             if (!ra3d.member.hasPermission('MANAGE_ROLES')) return ra3d.channel.sendMessage('`⚠  `[MANAGE_ROLES]` لا يوجد لديك صلاحية'); 
-              ra3d.channel.send(`✅ Created __${args}__ Colors`);
-                  setInterval(function(){})
-                    let count = 0;
-                    let ecount = 0;
-          for(let x = 1; x  `${parseInt(args)+1}`; x++){
-            ra3d.guild.createRole({namex,
-              color 'RANDOM'})
-              }
-            }
-       });
-`
 client.on('message',  (message) => {
         if(message.content.startsWith('.punch')) {
   let user = message.mentions.users.first();
