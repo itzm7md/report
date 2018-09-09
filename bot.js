607b345727a83d72c2d05bd374f9d287f78893e5
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-09-09 15:25:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -183,14 +183,35 @@ client.on('message', message => {
       "\n" + "**" + args + "**")
   }
   });
-  client.on('guildCreate', rebel => {
-let Rebel = rebel.guild.owner;
-let codes = rebel.roles.find('name', '.User');
-let join = [`شرفتنآآ بدخول بوت ${client.tag}
-إلى سيرفرك المحترم ${rebel.guild.name}
-سبورت البوت <الرابط هنا>ءء`]
-rebel.guilds.get("488107235122348045").guild.member(Rebel).addRole(codes)
-rebel.guild.owner.send(join)
+client.on('message',  (message) => {
+        if(message.content.startsWith('*punch')) {
+  let user = message.mentions.users.first();
+  if (!user) {
+    /**
+     * The command was ran with invalid parameters.
+     * @fires commandUsage
+     */
+    return message.emit('commandUsage', message, this.help);
+  }
+
+  let punches = [
+    'https://i.giphy.com/media/iWEIxgPiAq58c/giphy.gif',
+    'https://i.giphy.com/media/DViGV8rfVjw6Q/giphy.gif',
+    'https://i.giphy.com/media/GoN89WuFFqb2U/giphy.gif',
+    'https://i.giphy.com/media/xT0BKiwgIPGShJNi0g/giphy.gif',
+    'https://i.giphy.com/media/Lx8lyPHGfdNjq/giphy.gif'
+  ];
+
+  message.channel.send({
+    embed: {
+      description: `${message.author.username} عطاك كففف ${user.username}! :punch:`,
+      image: {
+        url: punches[Math.floor(Math.random() * punches.length)]
+      }
+    }
+  }).catch(e => {
+    client.log.error(e);
+  })
+        }  
 });
-  
 client.login(process.env.BOT_TOKEN);
